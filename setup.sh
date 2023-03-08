ef27b412d87d0e1c1b56ea93a156b11ee9401ced
Dane Walton <dawalton.microsoft.com>
2023-03-08 17:11:18 -0500

    add fzf

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mv .git .gitignore .vimrc .tmux.conf README .zshrc ~/
+cp .vimrc .tmux.conf .zshrc ~/
 
 sudo apt-get update
 sudo apt-get upgrade
@@ -10,6 +10,10 @@ sudo apt-get install -y build-essential git tmux vim clang-format libcmocka-dev 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+# Install FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 # Control P install
 mkdir -p ~/.vim/pack/plugins/start
@@ -25,5 +29,3 @@ ssh-keygen -t ed25519 -C "dawalton@microsoft.com"
 popd
 
 source ~/.zshrc
-
-
